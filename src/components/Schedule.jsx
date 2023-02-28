cfd714c1a9b1e8e7f6b2e3e496a024f8d489da2b
Morgan Jackson <mbj4school@gmail.com>
2023-02-28 13:32:34 -0600

    schedule updated

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -14,8 +14,11 @@ const Schedule = () => {
         <Text fontSize="6xl" fontFamily="monospace">
           Schedule
         </Text>
+        <Text fontSize="xl" fontFamily="monospace" mb="4vh">
+          All times are in CST
+        </Text>
         <Flex display="flex" flexDirection="row" alignItems="top" wrap="wrap" maxW="100%" justify="center">
-          <Box>
+          <Box minW="25vw">
             <Text
               mt="-5px"
               mb="0vh"
@@ -34,14 +37,22 @@ const Schedule = () => {
               fontSize="xl"
               fontFamily="monospace"
             >
-              5pm Check-In
+              5:00pm Check-In
               <br />
-              6pm Career Fair
+              6:00pm Career Fair
               <br />
-              7pm Opening Ceremony
+              6:00pm Dinner Begins
+              <br />
+              7:00pm Opening Ceremony
+              <br />
+              7:20pm Team Formation
+              <br />
+              7:30pm Hacking Begins
+              <br />
+              10:00pm Late Night Snack
             </Text>
           </Box>
-          <Box>
+          <Box minW="25vw">
             <Text
               mt="-5px"
               mb="0vh"
@@ -60,10 +71,20 @@ const Schedule = () => {
               fontSize="xl"
               fontFamily="monospace"
             >
-              Hacking Continues...
+              8:00am Breakfast
+              <br />
+              12:00pm Lunch
+              <br />
+              4:00pm Dev Post Due
+              <br />
+              6:00pm Dinner
+              <br />
+              10:00pm Late Night Snack
+              <br />
+              11:00pm Ping Pong Tournament
             </Text>
           </Box>
-          <Box>
+          <Box minW="25vw">
             <Text
               mt="-5px"
               mb="0vh"
@@ -82,11 +103,90 @@ const Schedule = () => {
               fontSize="xl"
               fontFamily="monospace"
             >
-              10am Hacking Ends
+              8:00am Breakfast
               <br />
-              11am Judging
+              10:00am Hacking Ends
               <br />
-              12pm Closing/Prizes
+              10:30am Judging Begins
+              <br />
+              12:00pm Closing/Awards
+              <br />
+              12:45pm SIUE Bus Departs
+            </Text>
+          </Box>
+        </Flex>
+        <Text fontSize="4xl" fontFamily="monospace" mb="4vh">
+          Tech Talks
+        </Text>
+        <Flex display="flex" flexDirection="row" alignItems="top" wrap="wrap" maxW="100%" justify="center">
+          <Box minW="25vw">
+            <Text
+              mt="-5px"
+              mb="0vh"
+              ml="5vmin"
+              mr="5vmin"
+              fontSize="3xl"
+              fontFamily="monospace"
+            >
+              Friday
+            </Text>
+            <Text
+              mt="-5px"
+              mb="3vh"
+              ml="5vmin"
+              mr="5vmin"
+              fontSize="xl"
+              fontFamily="monospace"
+            >
+              8:00pm Intro to TrinoDB
+              <br />
+              8:45pm Intro to TypeScript
+            </Text>
+          </Box>
+          <Box minW="25vw">
+            <Text
+              mt="-5px"
+              mb="0vh"
+              ml="5vmin"
+              mr="5vmin"
+              fontSize="3xl"
+              fontFamily="monospace"
+            >
+              Saturday
+            </Text>
+            <Text
+              mt="-5px"
+              mb="3vh"
+              ml="5vmin"
+              mr="5vmin"
+              fontSize="xl"
+              fontFamily="monospace"
+            >
+              10:00am TBA
+              <br />
+              2:30pm TBA
+            </Text>
+          </Box>
+          <Box minW="25vw">
+            <Text
+              mt="-5px"
+              mb="0vh"
+              ml="5vmin"
+              mr="5vmin"
+              fontSize="3xl"
+              fontFamily="monospace"
+            >
+              Sunday
+            </Text>
+            <Text
+              mt="-5px"
+              mb="3vh"
+              ml="5vmin"
+              mr="5vmin"
+              fontSize="xl"
+              fontFamily="monospace"
+            >
+              TBD
             </Text>
           </Box>
         </Flex>
