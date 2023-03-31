d8e9fa154b619363191eb5803274616a9857a712
Morgan Jackson <morjack@siue.edu>
2023-03-31 17:27:53 -0500

    schedule updated + ScheduleItem created

--- src/components/HomePage/Schedule.jsx
+++ src/components/HomePage/Schedule.jsx
@@ -1,6 +1,37 @@
 import { Text, Box, Flex } from "@chakra-ui/react";
 
 const Schedule = () => {
+
+  const ScheduleItem = ({ time, event }) => {
+    return (
+      <Flex direction="row">
+        <Text
+          ml="4vh"
+          mb="1vh"
+          mr="1vmin"
+          fontSize="xl"
+          fontFamily="monospace"
+          align="right"
+          width="25%"
+        >
+          {time}
+        </Text>
+        <Text
+          mr="4vh"
+          mb="1vh"
+          ml="5vmin"
+          fontSize="xl"
+          fontFamily="monospace"
+          align="left"
+          width="75%"
+        >
+          {event}
+        </Text>
+      </Flex>
+    );
+  }
+
+
   return (
     <Box
       mb="30vh"
@@ -10,29 +41,36 @@ const Schedule = () => {
       alignItems="center"
       maxW="80vw"
     >
-      <Box display="flex" flexDirection="column" alignItems="center" maxW="100%">
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        maxW="100%"
+        
+      >
         <Text fontSize="6xl" fontFamily="monospace">
           Schedule
         </Text>
         <Text fontSize="xl" fontFamily="monospace" mb="4vh">
           All times are in CST
         </Text>
-        <Flex display="flex" flexDirection="row" alignItems="top" wrap="wrap" maxW="100%" justify="center">
-          <Box minW="25vw">
-            <Text
-              mt="-5px"
-              mb="3vh"
-              ml="5vmin"
-              mr="5vmin"
-              fontSize="xl"
-              fontFamily="monospace"
-            >
-              Morning - Code Basics
-              <br />
-              Midday - Lunch
-              <br />
-              Afternoon - Code Project
-            </Text>
+        <Flex
+          display="flex"
+          flexDirection="row"
+          alignItems="top"
+          wrap="wrap"
+          maxW="100%"
+          justify="center"
+        >
+          <Box minW="36vw">
+            <ScheduleItem time="10:00 AM" event="Check-In Begins" />
+            <ScheduleItem time="10:15 AM" event="Fun & Games" />
+            <ScheduleItem time="10:30 AM" event="Welcome & Introductions" />
+            <ScheduleItem time="10:45 AM" event="Begin Working On Projects" />
+            <ScheduleItem time="12:15 PM" event="Lunch On Us" />
+            <ScheduleItem time="12:45 PM" event="Resume Working On Projects" />
+            <ScheduleItem time="2:15 PM" event="Coming To A Close" />
+            <ScheduleItem time="2:30 PM" event="Event Ends" />
           </Box>
         </Flex>
       </Box>
