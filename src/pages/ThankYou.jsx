e7b7e7cf57cfa6e8ae681f81206a3a6c482c6442
Cam McKay <cam.mckay9@gmail.com>
2024-02-14 09:29:00 -0600

    changed discord invite to permanent link

--- src/pages/ThankYou.jsx
+++ src/pages/ThankYou.jsx
@@ -11,7 +11,7 @@ const ThankYou = () => {
         <Flex direction="column" justify="center" gap="2vh" h="100vh">
           <Text>Thank you for registering for eHacks 2024!</Text>
           <Text>We will be in touch if there are issues with your registration info!</Text>
-          <Text><ChakraLink href="https://discord.gg/PRzCKeRQ" target="_blank" textDecoration="underline">Click to join</ChakraLink> the event Discord server so you don't miss any announcements!</Text>
+          <Text><ChakraLink href="https://discord.gg/GhAMupx3fS" target="_blank" textDecoration="underline">Click to join</ChakraLink> the event Discord server so you don't miss any announcements!</Text>
           <Text>If you have any questions check out our <ChakraLink href="/#faq" textDecoration="underline">FAQ</ChakraLink> or reach out at <ChakraLink href="mailto:siue.ehacks@gmail.com">siue.ehacks@gmail.com</ChakraLink></Text>
           <Link to="/">
             <Button variant="outline">Return to Homepage</Button>
