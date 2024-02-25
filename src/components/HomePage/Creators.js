86529abc3234d6c5e8935e60276c410b313b3255
Jared Christopher <jarchri@siue.edu>
2024-02-25 20:47:34 +0000

    Reordered the Organizers

--- src/components/HomePage/Creators.js
+++ src/components/HomePage/Creators.js
@@ -7,14 +7,6 @@ import JoeImage from "../../assets/headshots/Joe.jpg";
 export const creators = [
     {
         id: 1,
-        name: 'Jared Christopher',
-        role: 'Webmaster',
-        linkedin: 'https://www.linkedin.com/in/jaredchristopher/',
-        image: JaredImage
-    },
-
-    {
-        id: 2,
         name: 'Scott DeLozier',
         role: 'Director',
         linkedin: 'https://www.linkedin.com/in/scott-delozier-3a17a8209/',
@@ -22,19 +14,27 @@ export const creators = [
     },
 
     {
-        id: 3,
-        name: 'Matthew Lands',
-        role: 'Meal Organizer',
-        linkedin: 'https://www.linkedin.com/in/matthewlands/',
-        image: MattImage
-    },
-
-    {
-        id: 4,
+        id: 2,
         name: 'Camden McKay',
         role: 'Webmaster',
         linkedin: 'https://www.linkedin.com/in/camden-mckay-a3140b251/',
         image: LogLugImage
+    },
+
+    {
+        id: 3,
+        name: 'Jared Christopher',
+        role: 'Webmaster',
+        linkedin: 'https://www.linkedin.com/in/jaredchristopher/',
+        image: JaredImage
+    },
+
+    {
+        id: 4,
+        name: 'Matthew Lands',
+        role: 'Meal Organizer',
+        linkedin: 'https://www.linkedin.com/in/matthewlands/',
+        image: MattImage
     },
 
     {
